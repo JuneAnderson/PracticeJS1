0833c33aea7cd7b932521c95b6e3f28652ebea32
June Anderson <ktototam09@gmail.com>
2023-03-01 02:27:27 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+'use strict'; 
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+	count: numberOfFilms, 
+	movies: {},
+	actors: {}, 
+	genres: [], 
+	privat: false
+}; 
+ 
