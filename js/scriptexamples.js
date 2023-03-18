cd4e84c076185d6641bc9a18d1c6d933030972f6
June Anderson <ktototam09@gmail.com>
2023-03-18 20:30:25 +0100

    scriptexamples added

---
+++ js/scriptexamples.js
@@ -0,0 +1,24 @@
+'use strickt'; 
+
+let num = 50; 
+
+//цикл while пример 
+
+while (num <= 55) {
+	console.log(num); 
+	num++; 
+}
+
+//Пример циклы do 
+do {
+	console.log(num); 
+	num++; 
+}
+while (num <55); 
+
+
+//цикл for пример 
+for (let i = 1; i < 8; i ++){
+	console.log(i); 
+	num++; 
+}
