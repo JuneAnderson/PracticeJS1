84388482abdacd65efa6ee32036d5d50a0f31823
June Anderson <ktototam09@gmail.com>
2023-03-18 18:39:09 +0100

    continue practice

--- js/script.js
+++ js/script.js
@@ -1,38 +1,37 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 'use strict'; 
 
-// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// const personalMovieDB = {
-// 	count: numberOfFilms, 
-// 	movies: {},
-// 	actors: {}, 
-// 	genres: [], 
-// 	privat: false
-// }; 
+const personalMovieDB = {
+	count: numberOfFilms, 
+	movies: {},
+	actors: {}, 
+	genres: [], 
+	privat: false
+}; 
 
-// const a = prompt('Один из последних просмотренных фильмов?', ''),
-// 	b = prompt('На сколько оцените его?', ''),
-// 	c = prompt('Один из последних просмотренных фильмов?', ''),
-// 	d = prompt('На сколько оцените его?', ''); 
-      
-// personalMovieDB.movies[a] = b; 
-// personalMovieDB.movies[c] = d; 
+for (let i = 0; i < 2; i++) {
+	const a = prompt('Один из последних просмотренных фильмов?', ''),
+	      b = prompt('На сколько оцените его?', ''); 
 
-// console.log(personalMovieDB); 
-
-const num = 50; 
-
-switch (num) {
-case 49: 
-	console.log('Not right'); 
-	break; 
-case 100:
-	console.log('Too much'); 
-	break; 
-case 50: 
-	console.log('Right!'); 
-	break; 
-default: 
-	console.log('Nope'); 
-	break; 
+	if (a!=null&&b!=null&&a!=''&&b!=''&&a.length<50) {
+		personalMovieDB.movies[a] = b; 
+		console.log('Done!'); 
+	} else {
+		console.log('error');
+		i--; 
+	} 
 }
+
+if(personalMovieDB.count < 10) {
+	console.log('Просмотрено довольно мало фильмов'); 
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	console.log('Вы классический зритель'); 
+} else if (personalMovieDB.count >= 30) {
+	console.log('Вы киноман!'); 
+} else {
+	console.log('Произошла ошибка'); 
+}
+
+
